43bdb8d731c2e3a7dd5a00c6d3139a287d574f40
m-bhuvnesh <mbhuvnesh26@gmail.com>
2025-06-15 19:08:56 +0530

    Refactor routing in App component and enhance LoginPage and ProfilePage with form handling and state management

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,11 +6,12 @@ import ProfilePage from './pages/ProfilePage'
 
 const App = () => {
   return (
+    // <div className="bg-[url('./src/assets/bgImage.svg')] bg-contain">
     <div className="bg-[url('./src/assets/bgImage.svg')] bg-contain">
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='/login' element={<LoginPage />} />
-        <Route path='/profile ' element={<ProfilePage />} />
+        <Route path='/profile' element={<ProfilePage />} />
       </Routes>
     </div>
   )
--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -3,13 +3,24 @@ import assets from "../assets/assets";
 import { useState } from "react";
 
 const LoginPage = () => {
-  const [currState, setCurrState] = useState("Sign up");
+    const [currState, setCurrState] = useState("Sign up");
   const [fullName, setFullName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [bio, setBio] = useState("");
 
   const [isDataSubmitted, setIsDataSubmitted] = useState(false);
+  const onsubmitHandler = (e) => {
+    e.preventDefault();
+    if (currState === "Sign up" && !isDataSubmitted) {
+      setIsDataSubmitted(true);
+      return;
+    }
+    // else {
+    //   // Handle login logic here
+    //   console.log("Logging in with:", { email, password });
+    // }
+  };
   return (
     <div
       className="min-h-screen bg-cover bg-center flex 
@@ -19,21 +30,29 @@ const LoginPage = () => {
       <img src={assets.logo_big} className="w-[min(30vw,250px)]" alt="" />
 
       <form
-        action=""
+        onSubmit={onsubmitHandler}
         className="border-2 bg-white/8 text-white border-gray-500 
         p-6 flex flex-col gap-6 rounded-lg shadow-lg"
       >
         <h2 className="font-medium text-2xl flex justify-between items-center">
           {currState}
-          <img src={assets.arrow_icon} alt="" className="w-5 cursor-pointer" />
+          {isDataSubmitted && (
+            <img
+              src={assets.arrow_icon}
+              onClick={() => setIsDataSubmitted(false)}
+              alt=""
+              className="w-5 cursor-pointer"
+            />
+          )}
+          {/* <img src={assets.arrow_icon} alt="" className="w-5 cursor-pointer" /> */}
         </h2>
         {currState === "Sign up" && !isDataSubmitted && (
           <input
             onChange={(e) => setFullName(e.target.value)}
             value={fullName}
             type="text"
-            name=""
-            className="p-2 border-gray-500 rounded-md focus:outline-none"
+            name="fname"
+            className="p-2 border border-gray-500 rounded-md focus:outline-none "
             id=""
             placeholder="Full Name"
             required
@@ -45,8 +64,7 @@ const LoginPage = () => {
               onChange={(e) => setEmail(e.target.value)}
               value={email}
               type="email"
-              className="p-2 border border-gray-500 rounded-md focus:outline-none focus:ring-2 
-              focus:right-indigo-500"
+              className="p-2 border border-gray-500 rounded-md focus:outline-none "
               placeholder="Email Address"
               required
             />
@@ -54,8 +72,7 @@ const LoginPage = () => {
               onChange={(e) => setPassword(e.target.value)}
               value={password}
               type="password"
-              className="p-2 border border-gray-500 rounded-md focus:outline-none focus:ring-2 
-              focus:right-indigo-500"
+              className="p-2 border border-gray-500 rounded-md focus:outline-none "
               placeholder="Password"
               required
             />
@@ -83,6 +100,41 @@ const LoginPage = () => {
         <div className="flex items-center gap-2 text-sm text-gray-500">
           <input type="checkbox" />
           <p>Agree to the terms of use & privacy policy. </p>
+        </div>
+        <div className="flex flex-col gap-2">
+          {currState === "Sign up" ? (
+            <p
+              onClick={() => {
+                setCurrState("Login");
+                setIsDataSubmitted(false);
+              }}
+              className="text-sm text-gray-500"
+            >
+              Already have an account?
+              <span
+                onClick={() => {
+                  setCurrState("Sign up");
+                  setIsDataSubmitted(false);
+                }}
+                className="font-medium text-violet-500 ml-2 cursor-pointer"
+              >
+                Login here
+              </span>
+            </p>
+          ) : (
+            <p className="text-sm text-gray-500">
+              Create an account
+              <span
+                onClick={() => {
+                  setCurrState("Sign up");
+                  setIsDataSubmitted(false);
+                }}
+                className="font-medium text-violet-500 ml-2 cursor-pointer"
+              >
+                Click here
+              </span>
+            </p>
+          )}
         </div>
       </form>
     </div>
