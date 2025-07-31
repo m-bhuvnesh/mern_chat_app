c39cf5f98e3ab38b419d90759dda7106adab8804
m-bhuvnesh <mbhuvnesh26@gmail.com>
2025-07-31 23:14:05 +0530

    Fix import case sensitivity for HomePage component in App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useContext } from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
-import HomePage from "./pages/HomePage.JSX";
+import HomePage from "./pages/HomePage.jsx";
 import LoginPage from "./pages/LoginPage";
 import ProfilePage from "./pages/ProfilePage";
 import { Toaster } from "react-hot-toast";
